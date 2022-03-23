dae6cdd2eb4fedde644b07816d1243d23190cf3d
robert18x <robertzurawski14@gmail.com>
2022-03-23 17:44:41 +0100

    Added docker installation commands in script

--- scripts/run_in_docker.sh
+++ scripts/run_in_docker.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+
+sudo apt-get update && apt-get update -y && \
+sudo apt-get install docker
+
 sudo docker build -t vehicle_evolution:0.0.1 .
 sudo docker run --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" vehicle_evolution:0.0.1

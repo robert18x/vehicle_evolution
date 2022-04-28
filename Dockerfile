f44b46f4dbf1082bb976e4bb0d5830f801a92dcf
robert18x <robertzurawski14@gmail.com>
2022-04-28 22:22:57 +0200

    Updated required packages

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,12 @@ ARG DEBIAN_FRONTEND=noninteractive
 
 RUN apt-get update && apt-get update -y && \
     apt-get install -y gcc-10 g++-10 make gdb cppcheck clang-tidy clang-format ccache python3 python3-pip libgl-dev && \
+        pkg-config libx11-dev libx11-xcb-dev libfontenc-dev libice-dev libsm-dev libxau-dev libxaw7-dev libxcomposite-dev && \
+        libxcursor-dev libxdamage-dev libxdmcp-dev libxext-dev libxfixes-dev libxft-dev libxi-dev libxinerama-dev && \
+        libxkbfile-dev libxmu-dev libxmuu-dev libxpm-dev libxrandr-dev libxrender-dev libxres-dev libxss-dev libxt-dev && \
+        libxtst-dev libxv-dev libxvmc-dev libxxf86vm-dev xtrans-dev libxcb-render0-dev libxcb-render-util0-dev && \
+        libxcb-xkb-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-keysyms1-dev libxcb-randr0-dev libxcb-shape0-dev && \
+        libxcb-sync-dev libxcb-xfixes0-dev libxcb-xinerama0-dev xkb-data libxcb-dri3-dev uuid-dev libxcb-util-dev && \
     python3 -m pip install conan cmake && pip3 install --upgrade conan cmake
 
 ENV CC=/usr/bin/gcc-10
--- scripts/ubuntu_prepare_environment.sh
+++ scripts/ubuntu_prepare_environment.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
 sudo apt-get update && apt-get upgrade -y && \
-sudo apt-get install -y gcc-10 g++-10 make gdb cppcheck clang-tidy clang-format ccache python3 python3-pip \
-                        libgl-dev libice-dev pkg-config libstb-dev libfreetype-dev liballegro5-dev libglfw3-dev \
-                        freeglut3-dev libglew-dev libsdl2-dev libvulkan-dev libx11-xcb-dev libfontenc-dev libxaw7-dev \
-                        libxkbfile-dev libxmu-dev libxmuu-dev libxres-dev libxtst-dev libxvmc-dev libxcb-render-util0-dev \
-                        libxcb-xkb-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-keysyms1-dev libxcb-randr0-dev libxcb-shape0-dev \
-                        libxcb-sync-dev libxcb-xfixes0-dev libxcb-xinerama0-dev libxcb-dri3-dev libxcb-util-dev xorg-dev
+sudo apt-get install -y gcc-10 g++-10 make gdb cppcheck clang-tidy clang-format ccache python3 python3-pip libgl-dev && \
+    pkg-config libx11-dev libx11-xcb-dev libfontenc-dev libice-dev libsm-dev libxau-dev libxaw7-dev libxcomposite-dev && \
+    libxcursor-dev libxdamage-dev libxdmcp-dev libxext-dev libxfixes-dev libxft-dev libxi-dev libxinerama-dev && \
+    libxkbfile-dev libxmu-dev libxmuu-dev libxpm-dev libxrandr-dev libxrender-dev libxres-dev libxss-dev libxt-dev && \
+    libxtst-dev libxv-dev libxvmc-dev libxxf86vm-dev xtrans-dev libxcb-render0-dev libxcb-render-util0-dev && \
+    libxcb-xkb-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-keysyms1-dev libxcb-randr0-dev libxcb-shape0-dev && \
+    libxcb-sync-dev libxcb-xfixes0-dev libxcb-xinerama0-dev xkb-data libxcb-dri3-dev uuid-dev libxcb-util-dev && \
 python3 -m pip install conan cmake && pip3 install --upgrade conan cmake
 # sudo ln -s ~/.local/bin/cmake /usr/bin/cmake
-# libwayland-dev libxkbcommon-dev wayland-protocols extra-cmake-modules

266d446d994fce4f43f7a74ab70c488bd4f725e6
Jakub Ptasznik <quba2000@o2.pl>
2022-06-12 12:26:35 -0400

    final car tweaks

--- src/car.cpp
+++ src/car.cpp
@@ -9,7 +9,7 @@
 
 #include "draw.h"
 
-Car::Car(b2World* world) {
+Car::Car(b2World* wrld) {
     static std::mt19937 gen(0);
     static std::uniform_int_distribution<> disVertices(3, 10);
     static std::uniform_real_distribution<> dis(0.0, 8.0);
@@ -17,16 +17,61 @@ Car::Car(b2World* world) {
     int nVertices = disVertices(gen);
     std::vector<b2Vec2> vertices;
     vertices.reserve(maxVertices);
-    for (int i = 0; i < nVertices; ++i)
-    {
+    for (int i = 0; i < nVertices; ++i) {
         double x = dis(gen);
         double y = dis(gen);
         vertices[i].Set(x, y);
     }
-    b2PolygonShape chassis;
-    chassis.Set(vertices.data(), nVertices);
 
-    configuration = {nVertices, vertices};
+    static std::uniform_int_distribution<> disWheel(0, nVertices - 1);
+    int wheel1Vertex = disWheel(gen);
+    int wheel2Vertex = wheel1Vertex;
+    while (wheel1Vertex == wheel2Vertex) {
+        wheel2Vertex = disWheel(gen);
+    }
+
+    configuration = {nVertices, std::move(vertices), wheel1Vertex, wheel2Vertex};
+
+    world = wrld;
+
+    initCar();
+}
+
+Car::Car(b2World* wrld, Car::Configuration conf) {
+    world = wrld;
+    configuration = conf;
+
+    initCar();
+}
+
+Car::Car(Car&& other) : m_car(other.m_car), m_wheel1(other.m_wheel1), m_wheel2(other.m_wheel2), m_spring1(other.m_spring1), m_spring2(other.m_spring2) {
+    other.m_car = nullptr;
+    other.m_wheel1 = nullptr;
+    other.m_wheel2 = nullptr;
+    other.m_spring1 = nullptr;
+    other.m_spring2 = nullptr;
+}
+
+Car::~Car() {
+    world->DestroyJoint(m_spring1);
+    world->DestroyJoint(m_spring2);
+
+    world->DestroyBody(m_wheel1);
+    world->DestroyBody(m_wheel2);
+    world->DestroyBody(m_car);
+}
+
+void Car::CenterCamera() const {
+    g_camera.m_center.x = m_car->GetPosition().x;
+}
+
+auto Car::getConfiguration() -> Configuration {
+    return configuration;
+}
+
+void Car::initCar() {
+    b2PolygonShape chassis;
+    chassis.Set(configuration.vertices.data(), configuration.nVertices);
 
     b2FixtureDef carFD;
     carFD.shape = &chassis;
@@ -40,15 +85,8 @@ Car::Car(b2World* world) {
     m_car = world->CreateBody(&bd);
     m_car->CreateFixture(&carFD);
 
-    static std::uniform_int_distribution<> disWheel(0, nVertices - 1);
-    int wheel1Vertex = disWheel(gen);
-    int wheel2Vertex = wheel1Vertex;
-    while (wheel1Vertex == wheel2Vertex)
-    {
-        wheel2Vertex = disWheel(gen);
-    }
-    b2Vec2 wheel1Vec = vertices[wheel1Vertex];
-    b2Vec2 wheel2Vec = vertices[wheel2Vertex];
+    b2Vec2 wheel1Vec = configuration.vertices[configuration.wheel1Vertex];
+    b2Vec2 wheel2Vec = configuration.vertices[configuration.wheel2Vertex];
 
     b2CircleShape circle;
     circle.m_radius = 0.4f;
@@ -101,20 +139,4 @@ Car::Car(b2World* world) {
     m_spring2 = (b2WheelJoint*)world->CreateJoint(&jd);
 
     m_spring1->SetMotorSpeed(-25.0f);
-}
-
-Car::Car(Car&& other) : m_car(other.m_car), m_wheel1(other.m_wheel1), m_wheel2(other.m_wheel2), m_spring1(other.m_spring1), m_spring2(other.m_spring2) {
-    other.m_car = nullptr;
-    other.m_wheel1 = nullptr;
-    other.m_wheel2 = nullptr;
-    other.m_spring1 = nullptr;
-    other.m_spring2 = nullptr;
-}
-
-void Car::CenterCamera() const {
-    g_camera.m_center.x = m_car->GetPosition().x;
-}
-
-auto Car::getConfiguration() -> Configuration {
-    return configuration;
 }
--- src/car.h
+++ src/car.h
@@ -5,19 +5,23 @@
 
 #pragma once
 #include <box2d/box2d.h>
+
 #include <vector>
 
 class Car {
   public:
     struct Configuration {
-        int nVertces;
+        int nVertices;
         std::vector<b2Vec2> vertices;
+        int wheel1Vertex;
+        int wheel2Vertex;
     };
 
-    Car(b2World* world);
-    Car(b2World* world, Configuration configuration);
+    Car(b2World* wrld);
+    Car(b2World* wrld, Configuration conf);
     Car(const Car&) = delete;
     Car(Car&& other);
+    ~Car();
 
     void CenterCamera() const;
 
@@ -33,4 +37,8 @@ class Car {
     static constexpr auto carMask = 0x0004;
     static constexpr auto maxVertices = 10;
     Configuration configuration;
+
+    b2World* world;
+
+    void initCar();
 };
